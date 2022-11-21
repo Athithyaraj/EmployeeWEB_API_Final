c5b93637a45dfb3de6d632fad822058a6428886a
Athithyaraj <109848349+Athithyaraj@users.noreply.github.com>
2022-11-21 08:16:01 +0530

    update

--- EmployeeWEB_API_Final/Startup.cs
+++ EmployeeWEB_API_Final/Startup.cs
@@ -1,4 +1,4 @@
-﻿////using System.Linq;
+////using System.Linq;
 ////using EmployeeWEB_API_Final;
 ////using EmployeeWEB_API_Final.Models;
 //////using Microsoft.AspNetCore.OData.Builder;
@@ -62,77 +62,80 @@
 ////    }
 ////}
 //using AspNetCore3xODataSample.Web.Models;
-//using Microsoft.AspNetCore.OData.Extensions;
-//using Microsoft.AspNetCore.Builder;
-//using Microsoft.AspNetCore.Hosting;
-//using Microsoft.EntityFrameworkCore;
-//using Microsoft.Extensions.Configuration;
-//using Microsoft.Extensions.DependencyInjection;
-//using Microsoft.Extensions.Hosting;
-//using Microsoft.OData.Edm;
+using Microsoft.AspNetCore.OData.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OData.Edm;
+using Microsoft.AspNetCore.OData.Batch;
+using Microsoft.OData.ModelBuilder;
+using EmployeeWEB_API_Final.Models;
 
-//namespace EmployeeWEB_API_Final
-//{
-//    public class Startup
-//    {
-//        public Startup(IConfiguration configuration)
-//        {
-//            Configuration = configuration;
-//        }
+namespace EmployeeWEB_API_Final
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
 
-//        public IConfiguration Configuration { get; }
+        public IConfiguration Configuration { get; }
 
-//        // This method gets called by the runtime. Use this method to add services to the container.
-//        public void ConfigureServices(IServiceCollection services)
-//        {
-//            //services.AddControllers().AddNewtonsoftJson();
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddControllers().AddNewtonsoftJson();
 
-//            string connectionString = Configuration.GetConnectionString("ContactsDbConnectionStrings");
-//            services.AddDbContext<EmpContext>(options => options.UseNpgsql(connectionString));
-//            //services.AddTransient<IStorageBroker, StorageBroker>();
-//            //services.AddTransient<IStudentService, StudentService>();
-//            //services.AddTransient<ILogger, Logger<LoggingBroker>>();
-//            //services.AddTransient<ILoggingBroker, LoggingBroker>();
-//            services.AddOData();
-//        }
+            //string connectionString = Configuration.GetConnectionString("ContactsDbConnectionStrings");
+            //services.AddDbContext<EmpContext>(options => options.UseNpgsql(connectionString));
+            ////services.AddTransient<IStorageBroker, StorageBroker>();
+            ////services.AddTransient<IStudentService, StudentService>();
+            ////services.AddTransient<ILogger, Logger<LoggingBroker>>();
+            ////services.AddTransient<ILoggingBroker, LoggingBroker>();
+            //services.AddOData();
+        }
 
-//        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-//        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-//        {
-//            if (env.IsDevelopment())
-//            {
-//                app.UseDeveloperExceptionPage();
-//            }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
 
-//            app.UseODataBatching();
-//            app.UseHttpsRedirection();
-//            app.UseRouting();
-//            app.UseAuthorization();
+            //app.UseODataBatching();
+            //app.UseHttpsRedirection();
+            //app.UseRouting();
+            //app.UseAuthorization();
 
-//            app.UseEndpoints(endpoints =>
-//            {
-//                app.UseEndpoints(endpoints =>
-//                {
-//                    var defaultBatchHandler = new DefaultODataBatchHandler();
-//                    defaultBatchHandler.MessageQuotas.MaxNestingDepth = 2;
-//                    defaultBatchHandler.MessageQuotas.MaxOperationsPerChangeset = 10;
-//                    defaultBatchHandler.MessageQuotas.MaxReceivedMessageSize = 100;
-//                    endpoints.MapControllers();
-//                    endpoints.Select().Filter().Expand().OrderBy();
-//                    endpoints.MapODataRoute(
-//                        routeName: "api",
-//                        routePrefix: "api",
-//                        model: GetEdmModel(),
-//                        batchHandler: new DefaultODataBatchHandler());
-//                });
-//            });
-//        }
+            app.UseEndpoints(endpoints =>
+            {
+                app.UseEndpoints(endpoints =>
+                {
+                    var defaultBatchHandler = new DefaultODataBatchHandler();
+                    defaultBatchHandler.MessageQuotas.MaxNestingDepth = 2;
+                    defaultBatchHandler.MessageQuotas.MaxOperationsPerChangeset = 10;
+                    defaultBatchHandler.MessageQuotas.MaxReceivedMessageSize = 100;
+                    endpoints.MapControllers();
+                    //endpoints.Select().Filter().Expand().OrderBy();
+                    endpoints.MapODataServiceRoute(
+                        routeName: "api",
+                        routePrefix: "api",
+                        model: GetEdmModel(),
+                        batchHandler: defaultBatchHandler);
+                });
+            });
+        }
 
-//        private IEdmModel GetEdmModel()
-//        {
-//            var builder = new ODataConventionModelBuilder();
-//            builder.EntitySet<Employee>("Employee");
-//            return builder.GetEdmModel();
-//        }
-//    }
-//}
+        private IEdmModel GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Employee>("Employee");
+            return builder.GetEdmModel();
+        }
+    }
+}
